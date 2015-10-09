d979654812daee6e0ac8f67da7f1c04da1f1c908
Gordian Edenhofer <gordian.edenhofer@gmail.com>
2015-10-10 01:17:37 +0200

    Indent

--- Launch-And-Expose@xenatt.github.com/extension.js
+++ Launch-And-Expose@xenatt.github.com/extension.js
@@ -33,75 +33,74 @@ function disable() {
 }
 
 function init_expose() {
-
     expose_button = new St.Bin({ style_class: 'panel-button',
-                          reactive: true,
-                          can_focus: true,
-                          x_fill: true,
-                          y_fill: true,
-                          track_hover: true }); 
-    let expose_icon = new St.Icon({ 
-                          gicon: Gio.icon_new_for_string(expose_iconPath),
-                          style_class: 'system-status-icon expose-icon'});
-    // Add Class expose-icon for easy theme.
-    expose_button.set_child(expose_icon);
-    // Set icon size to 1.3 em.
-    expose_button.connect('button-press-event', function(){ 
-      if (!Main.overview.visible) {
-        Main.overview.toggle();
-      } else { 
-        if (Main.overview.viewSelector._showAppsButton.checked) {
-            Main.overview.viewSelector._showAppsButton.checked = false;
-        } else {
-              Main.overview.toggle();
-        } 
-      }
-    }); 
-}
+    reactive: true,
+    can_focus: true,
+    x_fill: true,
+    y_fill: true,
+    track_hover: true });
+    let expose_icon = new St.Icon({
+        gicon: Gio.icon_new_for_string(expose_iconPath),
+        style_class: 'system-status-icon expose-icon'});
+        // Add Class expose-icon for easy theme.
+        expose_button.set_child(expose_icon);
+        // Set icon size to 1.3 em.
+        expose_button.connect('button-press-event', function(){
+            if (!Main.overview.visible) {
+                Main.overview.toggle();
+            } else {
+                if (Main.overview.viewSelector._showAppsButton.checked) {
+                    Main.overview.viewSelector._showAppsButton.checked = false;
+                } else {
+                    Main.overview.toggle();
+                }
+            }
+        });
+    }
 
-function enable_expose() {
-    rb_index = Main.panel._rightBox.get_children();
-    Main.panel._rightBox.insert_child_at_index(expose_button, rb_index.length +1);
-}
+    function enable_expose() {
+        rb_index = Main.panel._rightBox.get_children();
+        Main.panel._rightBox.insert_child_at_index(expose_button, rb_index.length +1);
+    }
 
-function disable_expose() {
-    Main.panel._rightBox.remove_child(expose_button);
-}
+    function disable_expose() {
+        Main.panel._rightBox.remove_child(expose_button);
+    }
 
-function init_launchpad() {
-      launchpad_button = new St.Bin({ style_class: 'panel-button',
-                          reactive: true,
-                          can_focus: true,
-                          x_fill: true,
-                          y_fill: true,
-                          track_hover: true }); 
-    let launchpad_icon = new St.Icon({ 
-                            gicon: Gio.icon_new_for_string(launchpad_iconPath),
-    // Add Class launchpad-icon for easy theme.
-                            style_class: 'system-status-icon launchpad-icon' });
-    launchpad_button.set_child(launchpad_icon);
-    // Set icon size to 1.3 em.
-    launchpad_button.connect('button-press-event', function(){ 
-      if (!Main.overview.visible) {
-        Main.overview.toggle();Main.overview.viewSelector._showAppsButton.checked = true;
-      } else { 
-        if (Main.overview.viewSelector._showAppsButton.checked) {
-            Main.overview.viewSelector._showAppsButton.checked = false;
-            Main.overview.toggle();
-        } else {
-              Main.overview.viewSelector._showAppsButton.checked = true;
-        } 
-      }
-    }); 
-}
+    function init_launchpad() {
+        launchpad_button = new St.Bin({ style_class: 'panel-button',
+        reactive: true,
+        can_focus: true,
+        x_fill: true,
+        y_fill: true,
+        track_hover: true });
+        let launchpad_icon = new St.Icon({
+            gicon: Gio.icon_new_for_string(launchpad_iconPath),
+            // Add Class launchpad-icon for easy theme.
+            style_class: 'system-status-icon launchpad-icon' });
+            launchpad_button.set_child(launchpad_icon);
+            // Set icon size to 1.3 em.
+            launchpad_button.connect('button-press-event', function(){
+                if (!Main.overview.visible) {
+                    Main.overview.toggle();Main.overview.viewSelector._showAppsButton.checked = true;
+                } else {
+                    if (Main.overview.viewSelector._showAppsButton.checked) {
+                        Main.overview.viewSelector._showAppsButton.checked = false;
+                        Main.overview.toggle();
+                    } else {
+                        Main.overview.viewSelector._showAppsButton.checked = true;
+                    }
+                }
+            });
+        }
 
-function enable_launchpad() {
-    Main.panel._leftBox.insert_child_at_index(launchpad_button, 0);
-    activitiesButton = Main.panel.statusArea['activities'];
-    activitiesButton.container.hide();
-}
+        function enable_launchpad() {
+            Main.panel._leftBox.insert_child_at_index(launchpad_button, 0);
+            activitiesButton = Main.panel.statusArea['activities'];
+            activitiesButton.container.hide();
+        }
 
-function disable_launchpad() {
-    Main.panel._leftBox.remove_child(launchpad_button);
-    activitiesButton.container.show();
-}
+        function disable_launchpad() {
+            Main.panel._leftBox.remove_child(launchpad_button);
+            activitiesButton.container.show();
+        }
