aa5121eea9cda52b6c52cafb30e4e90fc9437e44
Gordian Edenhofer <gordian.edenhofer@gmail.com>
2016-04-02 14:57:23 +0200

    Update to GNOME 3.20 + Indentation: Use tabs

--- RemovePanelAppMenu@evotex.ch/extension.js
+++ RemovePanelAppMenu@evotex.ch/extension.js
@@ -19,20 +19,20 @@ function init() {}
 * Enable the extension
 */
 function enable() {
-    let value = GLib.Variant.new('a{sv}', { 'Gtk/ShellShowsAppMenu': GLib.Variant.new('i', 0) });
-    let xsetting = new Gio.Settings({ schema: 'org.gnome.settings-daemon.plugins.xsettings' });
-    xsetting.set_value('overrides', value);
+	let value = GLib.Variant.new('a{sv}', { 'Gtk/ShellShowsAppMenu': GLib.Variant.new('i', 0) });
+	let xsetting = new Gio.Settings({ schema: 'org.gnome.settings-daemon.plugins.xsettings' });
+	xsetting.set_value('overrides', value);
 
-    Panel._leftBox.get_child_at_index(1).hide();
+	Panel._leftBox.get_child_at_index(1).hide();
 }
 
 /**
 * Disable the extension
 */
 function disable() {
-    Panel._leftBox.get_child_at_index(1).show();
+	Panel._leftBox.get_child_at_index(1).show();
 
-    let value = GLib.Variant.new('a{sv}', { 'Gtk/ShellShowsAppMenu': GLib.Variant.new('i', 1) });
-    let xsetting = new Gio.Settings({ schema: 'org.gnome.settings-daemon.plugins.xsettings' });
-    xsetting.set_value('overrides', value);
+	let value = GLib.Variant.new('a{sv}', { 'Gtk/ShellShowsAppMenu': GLib.Variant.new('i', 1) });
+	let xsetting = new Gio.Settings({ schema: 'org.gnome.settings-daemon.plugins.xsettings' });
+	xsetting.set_value('overrides', value);
 }
